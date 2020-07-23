fbc60c092ed1f122d33ff9b39ade3734d587cece
yuxi <xueyx1619@outloo.com>
2020-07-24 01:22:40 +0800

    Initial Commit

---
+++ practice1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice1
+//
+//  Created by 瑜夕 on 2020/7/24.
+//  Copyright © 2020 alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
